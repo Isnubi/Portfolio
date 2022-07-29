40f3652ee7433e99298c58f3fc7863886ca9dca7
Louis GAMBART <louis.gambart77270@gmail.com>
2022-07-29 17:51:42 +0200

    Add automatic year change for the footer and fix link problem in it too

--- public/assets/dist/php/footer_with_class.php
+++ public/assets/dist/php/footer_with_class.php
@@ -1,12 +1,10 @@
 <footer id="index_foot"> <!--Pied de page-->
     <a href="https://www.linkedin.com/in/louis-gambart" title="Lien vers mon profil Linkedin" target="_blank">
-        <img alt="Logo LinkedIn" src="https://i.ibb.co/yq6X4Pv/linkedin.png" width="24px" height="24px"/>
-    </a>
-    &#183; <!--Séparateur-->
+        <img alt="Logo LinkedIn" src="https://i.ibb.co/yq6X4Pv/linkedin.png" width="24px" height="24px"/></a>
+    • <!--Séparateur-->
     <a href="https://github.com/Isnubi" title="Lien vers mon profil Github" target="_blank">
-        <img alt="Logo Github" src="https://i.ibb.co/Dp4dfRK/github.png" width="24px" height="24px"/>
-    </a>
-    <p>&copy; Louis GAMBART | Tous droits réservés<br>
+        <img alt="Logo Github" src="https://i.ibb.co/Dp4dfRK/github.png" width="24px" height="24px"/></a>
+    <p>&copy; Louis GAMBART | Tous droits réservés • <?php include($_SERVER['DOCUMENT_ROOT'].'/public/assets/dist/php/get_current_year.php'); ?><br>
         <a class ="lien" href="/pages/mentions_legales.php">Mentions légales</a></p>
 </footer>
 <script src="/public/assets/dist/js/bootstrap.bundle.min.js"></script> <!--Script JS pour le menu déroulant-->
