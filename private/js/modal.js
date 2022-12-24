232fb006d303a95a8d467c00f8a114aab1156b52
Louis GAMBART <louis.gambart77270@gmail.com>
2022-12-24 18:29:12 +0100

    Refactor js modal code

--- private/js/modal.js
+++ private/js/modal.js
@@ -10,18 +10,6 @@ for (let i = 0; i < a.length; i++) {
     }
 }
 
-for (let i = 0; i < spans.length; i++) {
-    spans[i].onclick = function() {
-        for (let index in modals) {
-            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-        }
-    }
-}
+for (let i = 0; i < spans.length; i++) { spans[i].onclick = function() { for (let index in modals) { if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none"; } } }
 
-window.onclick = function(event) {
-    if (event.target.classList.contains('modal')) {
-        for (let index in modals) {
-            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-        }
-    }
-}
+window.onclick = function(event) { if (event.target.classList.contains('modal')) { for (let index in modals) { if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none"; } } }
