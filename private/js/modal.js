8db3e7fd6fe925e15ecfe4950a45de1e3bcab3d8
Louis GAMBART <louis.gambart77270@gmail.com>
2022-12-27 13:23:48 +0100

    Refactor code Add blur class to main container when open modal

--- private/js/modal.js
+++ private/js/modal.js
@@ -1,15 +1,37 @@
 let a = document.querySelectorAll("a.modal-trigger");
 let modals = document.querySelectorAll('.modal');
 let spans = document.getElementsByClassName("close");
+let container = document.querySelector('.container');
 
 for (let i = 0; i < a.length; i++) {
     a[i].onclick = function(e) {
         e.preventDefault();
-        let modal = document.querySelector(e.target.getAttribute('href'));
-        modal.style.display = "block";
+        container.classList.add('is-blurred');
+        setTimeout(function() {
+            let modal = document.querySelector(e.target.getAttribute('href'));
+            modal.style.display = "block";
+        }, 100);
     }
 }
 
-for (let i = 0; i < spans.length; i++) { spans[i].onclick = function() { for (let index in modals) { if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none"; } } }
+for (let i = 0; i < spans.length; i++) {
+    spans[i].onclick = function() {
+        for (let index in modals) {
+            if (typeof modals[index].style !== 'undefined') {
+                modals[index].style.display = "none";
+                container.classList.remove('is-blurred');
+            }
+        }
+    }
+}
 
-window.onclick = function(event) { if (event.target.classList.contains('modal')) { for (let index in modals) { if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none"; } } }
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        for (let index in modals) {
+            if (typeof modals[index].style !== 'undefined') {
+                modals[index].style.display = "none";
+                container.classList.remove('is-blurred');
+            }
+        }
+    }
+}
