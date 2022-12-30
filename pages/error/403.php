da859e772ff2f27af5c29d09eaea9c223a174c53
Louis GAMBART <louis.gambart77270@gmail.com>
2022-12-30 14:28:16 +0100

    Clean code of error pages

--- pages/error/403.php
+++ pages/error/403.php
@@ -1,18 +1,16 @@
 <!doctype html>
 <html lang="fr">
-<?php include($_SERVER['DOCUMENT_ROOT'].'/private/php/header.php');?>
-<body>
-<div id="filter"></div>
-<div id="banner"></div>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/private/php/navbar.php');?>
-<main class="container">
-    <div class="row center">
-        <div class="col-12">
-            <h1>Error 403</h1>
-            <p>Access denied</p>
-        </div>
-    </div>
-</main>
-</body>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/private/php/footer.php');?>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/private/php/header.php');?>
+    <body>
+        <div id="filter"></div>
+        <div id="banner"></div>
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/private/php/navbar.php');?>
+        <main class="container">
+            <div class="center">
+                <h1>Error 403</h1>
+                <p>Access denied</p>
+            </div>
+        </main>
+    </body>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/private/php/footer.php');?>
 </html>
