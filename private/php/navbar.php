07f63da6df018f188ff601b8f8c483c15a581061
Louis GAMBART <louis.gambart77270@gmail.com>
2022-12-30 10:20:58 +0100

    Rename css attribute to not override with another one (projects)

--- private/php/navbar.php
+++ private/php/navbar.php
@@ -6,11 +6,11 @@
         <div class="menu">
             <li><a href="/index.php">Home</a></li>
             <li><a href="/pages/about.php">About</a></li>
-            <li class="projects">Projects
+            <li class="projects-item">Projects
                 <ul class="dropdown">
-                    <li><a href="/pages/projects.php#project1">Project 1</a></li>
-                    <li><a href="/pages/projects.php#project2">Project 2</a></li>
-                    <li><a href="/pages/projects.php#project3">Project 3</a></li>
+                    <li><a href="/pages/projects.php#portfolio">Portfolio</a></li>
+                    <li><a href="/pages/projects.php#feurbot">FeurBot</a></li>
+                    <li><a href="/pages/projects.php#twt">Technology Watch Tool</a></li>
                 </ul>
             </li>
             <li><a href="/pages/contact.php">Contact</a></li>
